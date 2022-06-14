72b86cdd688c58650d81c27b277ac98efa720999
Eary Ortiz <eary117@hotmail.com>
2022-06-14 16:12:36 -0500

    Added sweet library

---
+++ LeaveManagement/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Deafult number o days")]
+        [Required]
+        public string Name { get; set; }
+        
+        [Display(Name = "Deafult number o days")]
+        [Required]
+        [Range(1, 25, ErrorMessage ="Please enter a valid number")]
+        public int DefaultDays { get; set; }
+    }
+}
